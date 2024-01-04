b7dddf6a8832d0477854fd35461a771323bf7373
bibhs0401 <bibhushabibhs@gmail.com>
2024-01-04 16:49:04 +0545

    produces continous data until manual interruption

--- producer.py
+++ producer.py
@@ -11,23 +11,26 @@ conf = {
     'bootstrap.servers': 'localhost:9092',
     'client.id': 'python-producer'
 }
+
 # Kafka producer function
 def produce():
     producer = Producer(conf)
-    # Produce some dummy data to Kafka topic
-    netflix_data = {
-        'userid': fake.uuid4(),
-        'user_location': random.choice(['Nepal', 'USA', 'India', 'China','Belgium', 'Canada','Switzerland']),
-        'channelid': fake.random_int(min=1, max=50),
-        'genre': random.choice(['thriller', 'comedy', 'romcom', 'fiction']),
-        'lastactive': fake.date_time_between(start_date='-10m', end_date='now').isoformat(),
-        'title': fake.name(),
-        'watchfrequency': fake.random_int(min=1, max=10),
-        'etags': fake.uuid4()
-    }
-    producer.produce('streamTopic', value=json.dumps(netflix_data))
-    print(netflix_data)
-    time.sleep(1)
-    producer.flush()
-    
+    while True:  # Run indefinitely until manually stopped
+        # Produce some dummy data to Kafka topic
+        netflix_data = {
+            'userid': fake.uuid4(),
+            'user_location': random.choice(['Nepal', 'USA', 'India', 'China', 'Belgium', 'Canada', 'Switzerland']),
+            'channelid': fake.random_int(min=1, max=50),
+            'genre': random.choice(['thriller', 'comedy', 'romcom', 'fiction']),
+            'lastactive': fake.date_time_between(start_date='-10m', end_date='now').isoformat(),
+            'title': fake.name(),
+            'watchfrequency': fake.random_int(min=1, max=10),
+            'etags': fake.uuid4()
+        }
+        producer.produce('streamTopic', value=json.dumps(netflix_data))
+        print(netflix_data)
+        time.sleep(1)
+        producer.flush()
+
+# Call the producer function
 produce()
